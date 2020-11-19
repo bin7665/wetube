461205d78b31739ec9af43d7069519f345760ee2
bin7665 <52473356+bin7665@users.noreply.github.com>
2020-11-19 21:06:47 +0900

    middleware 적용(cookieparser, bodyparser, helmet, morgan

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // const express = require("express");
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -10,6 +15,12 @@ const handleListening = () =>
 const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
