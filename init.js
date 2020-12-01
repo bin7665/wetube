975e8caa4d87a9be45b2052c3efd744177fd6955
bin7665 <bin7665@gmail.com>
2020-12-01 09:18:33 +0900

    Revert "mongo db 연결 및 schema 추가"
    This reverts commit 893090799907707bae00a9f24a6d6a62a5757628.

--- init.js
+++ init.js
@@ -1,10 +1,6 @@
-import "./db";
 import app from "./app";
-import dotenv from "dotenv";
-dotenv.config();
-import "./models/Video";
 
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 const handleListening = () =>
   console.log("✅ Listening on: http://localhost:" + PORT);
 
